584ed1967f10ab1614e3e095d284cd954adb349b
AO Industries, Inc. <support@aoindustries.com>
2018-04-28 02:43:37 -0500

    Added csh extension to open in NetBeans.

--- src/main/java/com/semanticcms/openfile/servlet/OpenFile.java
+++ src/main/java/com/semanticcms/openfile/servlet/OpenFile.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-openfile-servlet - SemanticCMS desktop integration mode for local content creation in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -251,6 +251,7 @@ final public class OpenFile {
 						//};
 					} else if(
 						"c".equals(extension)
+						|| "csh".equals(extension)
 						|| "h".equals(extension)
 						|| "java".equals(extension)
 						|| "jsp".equals(extension)
