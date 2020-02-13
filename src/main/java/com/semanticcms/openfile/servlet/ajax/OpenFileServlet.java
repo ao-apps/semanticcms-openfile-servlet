7994b1c01970c1efd85050b818bc7f38edc926d1
AO Industries, Inc. <support@aoindustries.com>
2020-02-13 14:44:37 -0600

    Using constants from new ContentType class.

--- src/main/java/com/semanticcms/openfile/servlet/ajax/OpenFileServlet.java
+++ src/main/java/com/semanticcms/openfile/servlet/ajax/OpenFileServlet.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-openfile-servlet - SemanticCMS desktop integration mode for local content creation in a Servlet environment.
- * Copyright (C) 2013, 2014, 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2016, 2017, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,6 +22,7 @@
  */
 package com.semanticcms.openfile.servlet.ajax;
 
+import com.aoindustries.io.ContentType;
 import com.aoindustries.net.DomainName;
 import com.aoindustries.net.Path;
 import com.aoindustries.validation.ValidationException;
@@ -69,7 +70,7 @@ public class OpenFileServlet extends HttpServlet {
 			);
 			// Write output
 			response.resetBuffer();
-			response.setContentType("application/xml");
+			response.setContentType(ContentType.XML);
 			response.setCharacterEncoding(ENCODING.name());
 			PrintWriter out = response.getWriter();
 			out.print("<?xml version=\"1.0\" encoding=\"");
