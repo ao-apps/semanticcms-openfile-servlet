ba03bec098031a02b16e4fb9005ae6c922ff3d7a
AO Industries, Inc <support@aoindustries.com>
2021-10-01 02:51:41 -0500

    Type-safe and scoped Servlet API attributes

--- src/main/java/com/semanticcms/openfile/servlet/OpenFile.java
+++ src/main/java/com/semanticcms/openfile/servlet/OpenFile.java
@@ -26,6 +26,7 @@ import com.aoapps.lang.ProcessResult;
 import com.aoapps.lang.io.FileUtils;
 import com.aoapps.net.DomainName;
 import com.aoapps.net.Path;
+import com.aoapps.servlet.attribute.ScopeEE;
 import com.aoapps.servlet.ServletUtil;
 import com.semanticcms.core.controller.Book;
 import com.semanticcms.core.controller.ResourceRefResolver;
@@ -57,7 +58,8 @@ final public class OpenFile {
 
 	private static final String ENABLE_INIT_PARAM = OpenFile.class.getName() + ".enabled";
 
-	private static final String FILE_OPENERS_APPLICATION_ATTRIBUTE = OpenFile.class.getName() + ".fileOpeners";
+	private static final ScopeEE.Application.Attribute<ConcurrentMap<String, FileOpener>> FILE_OPENERS_APPLICATION_ATTRIBUTE =
+		ScopeEE.APPLICATION.attribute(OpenFile.class.getName() + ".fileOpeners");
 
 	@WebListener
 	public static class Initializer implements ServletContextListener {
@@ -72,13 +74,7 @@ final public class OpenFile {
 	}
 
 	private static ConcurrentMap<String, FileOpener> getFileOpeners(ServletContext servletContext) {
-		@SuppressWarnings("unchecked")
-		ConcurrentMap<String, FileOpener> fileOpeners = (ConcurrentMap<String, FileOpener>)servletContext.getAttribute(FILE_OPENERS_APPLICATION_ATTRIBUTE);
-		if(fileOpeners == null) {
-			fileOpeners = new ConcurrentHashMap<>();
-			servletContext.setAttribute(FILE_OPENERS_APPLICATION_ATTRIBUTE, fileOpeners);
-		}
-		return fileOpeners;
+		return FILE_OPENERS_APPLICATION_ATTRIBUTE.context(servletContext).computeIfAbsent(__ -> new ConcurrentHashMap<>());
 	}
 
 	/**
@@ -132,7 +128,7 @@ final public class OpenFile {
 
 	/**
 	 * Registers a file opener.
-	 * 
+	 *
 	 * @param  extensions  The simple extensions, in lowercase, not including the dot, such as "dia"
 	 */
 	public static void addFileOpener(ServletContext servletContext, FileOpener fileOpener, String ... extensions) {
@@ -146,7 +142,7 @@ final public class OpenFile {
 
 	/**
 	 * Removes file openers.
-	 * 
+	 *
 	 * @param  extensions  The simple extensions, in lowercase, not including the dot, such as "dia"
 	 */
 	public static void removeFileOpener(ServletContext servletContext, String ... extensions) {
